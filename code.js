a719011f9228c3cbd4694f437e6adadf7d6a95dc
TylerJohnson77 <tjjohnson120@my.wctc.edu>
2021-05-05 21:32:39 -0500

    Added an array to store student objects, and sorting to sort them

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
+let studentArray = [];
+let student;
+
 $(document).ready(function ()
 {
     $("form").on("submit",calculation);
+    $("button#sortName").on("click", sortName);
+    $("button#sortGrade").on("click", sortGrade);
 });
 
 function calculation(event)
@@ -9,13 +14,15 @@ function calculation(event)
 
     //Create a student object
 
-    let student =
+    student =
         {
             firstName: $("#firstName").val(),
             lastName: $("#lastName").val(),
             pointsEarned: $("#pointsEarned").val(),
             totalPoints: $("#totalPoints").val()
         };
+
+    //store in the student array
 
     //Calculate the percent
 
@@ -46,10 +53,82 @@ function calculation(event)
         letterGrade = "F";
     }
 
+    student.percent = percent;
+    studentArray.push(student);
+
     //Print out the results
 
     $("#firstNameOutput").text(`First Name: ${student.firstName}`);
     $("#lastNameOutput").text(`Last Name: ${student.lastName}`);
     $("#percent").text(`Score: ${percent}%`);
     $("#letterGrade").text(`Final Grade: ${letterGrade}`);
+}
+
+
+function sortByName(a, b)
+{
+    if(a.firstName < b.firstName)
+    {
+        return -1;
+    }
+    else if(a.firstName > b.firstName)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+function sortByGrade(a, b)
+{
+    if(a.percent > b.percent)
+    {
+        return -1;
+    }
+    else if(a.percent < b.percent)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+function sortName()
+{
+    studentArray.sort(sortByName);
+    printArrayName();
+}
+
+function sortGrade()
+{
+    studentArray.sort(sortByGrade);
+    printArrayGrade();
+}
+
+function printArrayName()
+{
+    $("p#sortByName").empty();
+
+    for(let i = 0; i < studentArray.length; i++)
+    {
+        let output = studentArray[i].firstName + ", " + studentArray[i].lastName + ", " + studentArray[i].percent;
+        $("p#sortByName").append(output);
+        $("p#sortByName").append("<br>");
+    }
+}
+
+function printArrayGrade()
+{
+    $("p#sortByGrade").empty();
+
+    for(let i = 0; i < studentArray.length; i++)
+    {
+        let output = studentArray[i].firstName + ", " + studentArray[i].lastName + ", " + studentArray[i].percent;
+        $("p#sortByGrade").append(output);
+        $("p#sortByGrade").append("<br>");
+    }
 }
